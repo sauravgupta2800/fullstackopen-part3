7528dfd62af5e9387715b72f4f3ba02ab672ec1a
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-17 15:25:45 +0530

    adding practice server

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [{ id: 1, content: "ab" }];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.send(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.status(200).json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
+  console.log("note: ", note);
+  notes.push({ id: notes.length + 1, ...note });
+  response.status(201).send(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
